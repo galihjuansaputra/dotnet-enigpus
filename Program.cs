a862e93ab768c0bba614359c706288c55cdbfcfa
Galih Prastya Juansaputra <galih.juansaputra@gmail.com>
2024-06-10 17:46:43 +0700

    initial project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using dotnet_enigpus.view;
+
+namespace dotnet_enigpus;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        View view= new View();
+        view.Menu();
+    }
+}
